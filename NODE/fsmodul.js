880b294ada92e51d2ccf6d2f1df6c2f2ff32d7a3
Azamat-JS <azamatielts987@gmail.com>
2024-12-09 16:48:47 +0500

    async and sync

--- NODE/fsmodul.js
+++ NODE/fsmodul.js
@@ -10,30 +10,38 @@
 // )
 
 
-//---------------- fs synchronus ----------
-
+//---------------- fs asynchronus ---------
 const {readFile, writeFile} = require('fs')
+
+console.log('start');
+
 
 readFile('./content/first.txt', 'utf-8',(err, result)=>{
     if(err){
         console.log(err);
         return
     }
-const first = result;
-readFile('/content/second.txt', 'utf-8', (err, result)=>{
+const first = result
+readFile('./content/subfolder/second.txt', 'utf-8', (err, result)=>{
     if(err){
         console.log(err);
         return
     }
-    const first = result
-    writeFile('./content/result-sync.txt', `here is the result : ${first}, ${second}`, (err, result)=>{
+    const second = result
+   
+    
+    writeFile(
+        './content/result-sync.txt', 
+        `here is the result : ${first}, ${second}`, (err, result)=>{
         if(err){
             console.log(err);
             return
         }
-        const second = result
+        // console.log(result)
+       setTimeout(()=>{  console.log('done') }, 1000)
         
-    }),
-    {flag: 'a'}
+    })
 })
 })
+console.log('next');
+
