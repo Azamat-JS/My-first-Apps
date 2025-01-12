26323139ef7c7490634c8e73ab958ca95fc6a34f
Azamat-JS <azamatielts987@gmail.com>
2025-01-12 17:11:10 +0500

    added TS projects

---
+++ NAVBAR APP/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('./public'))
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>resourse not found</h1>')
+})
+
+app.listen(5001, () =>{
+    console.log('server is running on the port 5001')})
